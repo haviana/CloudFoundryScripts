518e1a7dd58a2c59cc04f1c4f61c2e6a2af2b9c3
haviana <hugohaviana@gmail.com>
2019-06-14 20:39:11 +0100

    Script for managing VM machine added

---
+++ vmManaging.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Bash Menu Script for Virtual Machine control
+
+PS3='Please enter your choice: '
+options=("Option 1 - Start Virtual Machine" "Option 2 - Save Virtual Machine State" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1 - Start Virtual Machine")
+            echo "you chose choice 1"
+	    vboxmanage startvm $(bosh int state.json --path /current_vm_cid) --type headless
+            ;;
+        "Option 2 - Save Virtual Machine State")
+            echo "Save Virtual Machine State"
+            vboxmanage controlvm $(bosh int state.json --path /current_vm_cid) savestate
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
