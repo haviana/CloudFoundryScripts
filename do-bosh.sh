cd355b9616ab8866cbf7aebd27b08bd14f54f7df
haviana <hugohaviana@gmail.com>
2019-06-18 17:54:35 +0100

    Menu support for deploying created

--- do-bosh.sh
+++ do-bosh.sh
@@ -1,49 +1,70 @@
 #!/bin/bash
 
-state="--state=./state.json"
-if [ -z $1 ]; then
-  action="int"
-else
-  action=$1
-fi
-
 # if doing interpolate, then state is invalid option
-if [ "$action" == "int" ]; then
-  state=""
-fi
-
-echo Performing $action
-bosh $action \
+create_env ()
+{
+./systemVariables.sh
+state="--state=./state.json"
+  func_result="some result"
+  if [ "$action" == "int" ]; then
+   	state=""
+   fi
+   echo Performing $action $state
+   bosh $action \
     $state \
-    bosh-deployment/bosh.yml \
-    --vars-store=./creds.yml \
-    -o bosh-deployment/virtualbox/cpi.yml \
-    -o bosh-deployment/virtualbox/outbound-network.yml \
-    -o bosh-deployment/bosh-lite.yml \
-    -o bosh-deployment/bosh-lite-runc.yml \
-    -o bosh-deployment/uaa.yml \
-    -o bosh-deployment/credhub.yml \
-    -o bosh-deployment/jumpbox-user.yml \
-    -o override-size.yml \
-    -v director_name=VirtualBox-Director \
+    $BOSH_HOME/bosh-deployment/bosh.yml \
+    --vars-store=$BOSH_HOME/creds.yml \
+    -o $BOSH_HOME/bosh-deployment/virtualbox/cpi.yml \
+    -o $BOSH_HOME/bosh-deployment/virtualbox/outbound-network.yml \
+    -o $BOSH_HOME/bosh-deployment/bosh-lite.yml \
+    -o $BOSH_HOME/bosh-deployment/bosh-lite-runc.yml \
+    -o $BOSH_HOME/bosh-deployment/uaa.yml \
+    -o $BOSH_HOME/bosh-deployment/credhub.yml \
+    -o $BOSH_HOME/bosh-deployment/jumpbox-user.yml \
+    -o $BOSH_HOME/override-size.yml \
+    -v director_name=MGCB_DIRECTOR \
     -v internal_ip=192.168.50.2 \
     -v internal_gw=192.168.50.1 \
     -v internal_cidr=192.168.50.0/24 \
     -v outbound_network_name=NatNetwork \
-    --vars-file vars.yml \
+    --vars-file $BOSH_HOME/vars.yml 
+}
 #source ~/.profile
-#Interpolate admin password from creds.yml
-#bosh int ./creds.yml --path /admin_password
-#Create Bosh Alias to VirtualBox
-#bosh -e 192.168.50.2 alias-env virtualbox --ca-cert <(bosh int ./creds.yml --path /director_ssl/ca)
+echo "select the operation ************"
+echo "  1)Installation Info"
+echo "  2)Setup Bosh Cloud System"
+echo "  3)Delete Bosh Cloud System"
+echo "  4)Create ssh config files"
+echo "  5)Create creds setup"
+echo "  6)Cloud-config and Stemcell installation"
 
-# get key from creds file
-#bosh int creds.yml --path /jumpbox_ssh/private_key > jumpbox.key
-#chmod 600 jumpbox.key
+read n
+case $n in
+  1) echo "You chose Option 1"
+	action=int
+	create_env;;
+  2) echo "You chose Option 2"
+     action=create-env
+	create_env;;
 
-# For testing SSh connection to the vm
-# ssh jumpbox@192.168.50.6 -i jumpbox.key
-
-#To list the env created
-#bosh envs
-#bosh login
+  3) echo "You chose Option 3"
+	action=delete-env
+	create_env;;
+  4) echo "You chose Option 4"
+	#get key from creds file
+	#for testing ssh connection
+	#ssh jumpbox@192.168.50.6 -i jumpbox.key
+	bosh int creds.yml --path /jumpbox_ssh/private_key > jumpbox.key
+	chmod 600 jumpbox.key;;
+  5) echo "Interpolate admin password from creds.yml"
+	bosh int ./creds.yml --path /admin_password
+	bosh -e 192.168.50.2 alias-env virtualbox --ca-cert <(bosh int ./creds.yml --path /director_ssl/ca);;
+  6) echo "You chose Option 6"
+	yes Y | bosh update-cloud-config bosh-deployment/warden/cloud-config.yml
+	wget https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
+	bosh upload-stemcell bosh-warden-boshlite-ubuntu-trusty-go_agent
+	rm -f bosh-warden-boshlite-ubuntu-trusty-go_agent
+	#ssh jumpbox@192.168.50.6 -i jumpbox.key
+	;;
+  *) echo "invalid option";;
+esac
--- do-cfENV.sh
+++ do-cfENV.sh
@@ -5,7 +5,7 @@ echo Performing $1
 echo "Loading system variables"
 ./systemVariables.sh
 # if doing interpolate, then state is invalid option
-action=$1
+create_cf_env(){
 if [ "$action" == "int" ]; then
   state=""
 else
@@ -19,7 +19,7 @@ fi
 echo "$CF_DEPLOYMENT_HOME"
 bosh \
   -d cf \
- $1 \
+ $action \
   $CF_DEPLOYMENT_HOME/cf-deployment.yml \
   -o $CF_DEPLOYMENT_HOME/operations/bosh-lite.yml \
   -o $CF_DEPLOYMENT_HOME/operations/scale-to-one-az.yml \
@@ -31,3 +31,40 @@ else
  echo "Fail - Deleting cf deployment"
  bosh -d cf delete-deployment
 fi
+}
+echo "select the operation ************"
+echo "  1)CF Installation Info"
+echo "  2)Deploy CF"
+echo "  3)Undeploy CF"
+
+read n
+case $n in
+  1) echo "You chose Option 1"
+        action=int
+        create_cf_env;;
+  2) echo "You chose Option 2"
+     action=deploy
+        create_cf_env;;
+
+  3) echo "You chose Option 3"
+ 	bosh -d cf delete-deployment
+        ;;
+  4) echo "You chose Option 4"
+        #get key from creds file
+        #for testing ssh connection
+        #ssh jumpbox@192.168.50.6 -i jumpbox.key
+        bosh int creds.yml --path /jumpbox_ssh/private_key > jumpbox.key
+        chmod 600 jumpbox.key;;
+  5) echo "Interpolate admin password from creds.yml"
+        bosh int ./creds.yml --path /admin_password
+        bosh -e 192.168.50.2 alias-env virtualbox --ca-cert <(bosh int ./creds.yml --path /director_ssl/ca);;
+  6) echo "You chose Option 6"
+        yes Y | bosh update-cloud-config bosh-deployment/warden/cloud-config.yml
+        wget https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
+        bosh upload-stemcell bosh-warden-boshlite-ubuntu-trusty-go_agent
+        rm -f bosh-warden-boshlite-ubuntu-trusty-go_agent
+        #ssh jumpbox@192.168.50.6 -i jumpbox.key
+        ;;
+  *) echo "invalid option";;
+esac
+
